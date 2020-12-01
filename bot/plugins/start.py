2efc7a0510ea76e643e063cf9611be81f78f2c3d
meaall-com <67676419+meaall-com@users.noreply.github.com>
2020-12-01 22:48:09 +0300

    Create start.py

---
+++ bot/plugins/start.py
@@ -0,0 +1,48 @@
+from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
+
+from ..translations import Messages as tr
+
+from ..config import Config
+
+from ..utubebot import UtubeBot
+
+@UtubeBot.on_message(
+
+    Filters.private 
+
+    & Filters.incoming
+
+    & Filters.command('start')
+
+    & Filters.user(Config.AUTH_USERS)
+
+)
+
+async def _start(c, m):
+
+    await m.reply_chat_action("typing")
+
+    
+
+    await m.reply_text(
+
+        text=tr.START_MSG.format(m.from_user.first_name),
+
+        quote=True,
+
+        reply_markup=InlineKeyboardMarkup(
+
+            [
+
+                [
+
+                    InlineKeyboardButton('Proje Kanalına Katılın!', url='https://t.me/SoftCoreSupport')
+
+                ]
+
+            ]
+
+        )
+
+    )
+
